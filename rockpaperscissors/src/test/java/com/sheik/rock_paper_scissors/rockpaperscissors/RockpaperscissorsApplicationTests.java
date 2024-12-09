fe87d58b005f53f469a992d60cb206a16f13ea2a
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-12-09 18:58:27 -0500

    project initialized

---
+++ rockpaperscissors/src/test/java/com/sheik/rock_paper_scissors/rockpaperscissors/RockpaperscissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheik.rock_paper_scissors.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
